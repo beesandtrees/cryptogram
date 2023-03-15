9ca3abf30a4df1449762ccef88634f46e75d6827
Melissa McMurtrie <melissa@melissakate.com>
2023-03-15 21:29:58 +0000

    updated to not use api - Update 8 files

--- src/App.js
+++ src/App.js
@@ -1,117 +1,24 @@
-import React, { Component } from 'react';
-import $ from 'jquery';
+import React from "react";
+import Riddle from "./components/riddle";
 
-import Riddle from './components/riddle';
-import AuthorList from './components/authors';
+import { sonnets } from "./helpers/sonnets";
 
-// import {riddles} from './helpers/riddles';
-import {authors} from './helpers/authors';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      play: false,
-      authors: [],
-      authorsClassList: ['authors'],
-      authorName: 'Choose Author',
-      authorLookup: '',
-      riddle: ''
-    };
-  }
-
-  componentDidMount() {
-    this.alphabetizeAuthors();
-  }
-
-  alphabetizeAuthors() {
-    let originalList = authors;
-    let newAuthorList = [];
-
-    // loop through originalList and add to new list
-    // then sort the new list alphabetically
-    for (var i = 0; i < originalList.length; i++) {
-      var author = originalList[i];
-    // // first reverse to last name first
-      // turn name into an array
-      // reorder the array to make the last name first
-      var full = author.split(" ");
-      var lastName = full.splice(-1,1);
-      var firstName = full.join(" ");
-
-      // put the array back together as a string
-      var fullName = lastName[0] + ", " + firstName;
-
-      newAuthorList.push(fullName);
-    }
-
-    // // then sort new list
-    newAuthorList.sort();
-
-    // set state to alphabetized authors
-    this.setState({
-      authors: newAuthorList
-    });
-  }
-
-  selectAuthor(selected) {
-      var full = selected["author"].split(" ");
-      var lastName = full.splice(0,1).toString().slice(0, -1);
-      var firstName = full.join(" ");
-      var fullName = firstName + " " + lastName;
-      var combo = "Author - " + fullName;
-      this.setState({authorLookup: fullName});
-      this.setState({authorName: combo});
-      this.setState({authors: []});
-      this.setState({play: true});
-      this.selectQuote(fullName);
-
-      document.getElementsByClassName("main")[0].classList.add("active");
-      // set state to alphabetized authors
-  }
-
-  selectQuote(author) {
-    // for(var x = 0; x < riddles.length; x++) {
-    //   if(riddles[x].author === author) {
-    //     this.setState({riddle: riddles[x].riddle});
-    //     return;
-    //   }
-    // }
-    var _this = this;
-    $.ajax({
-      url: 'https://thundercomb-poetry-db-v1.p.mashape.com/author/' + author,
-      type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
-      data: {}, // Additional parameters here
-      dataType: 'json',
-      success: function(data) {
-        _this.setState({riddle: data[Math.ceil(Math.random()*data.length-1)]});
-      },
-      error: function(err) {
-        console.log(err.message);
-      },
-      beforeSend: function(xhr) {
-        xhr.setRequestHeader("X-Mashape-Authorization", "ZqlZVIAPaEmsheL720EWgG23NjVap1n1cdJjsnYkpYU6POqo45"); // Enter here your Mashape key
-      }
-    });
-  }
-
-  resetApplication() {
+const App = () => {
+  const resetApplication = () => {
     document.getElementsByClassName("main")[0].classList.remove("active");
-    this.alphabetizeAuthors();
-    this.setState({play: false});
-    this.setState({authorLookup: ''});
-    this.setState({authorName: 'Choose Author'});
-    this.setState({riddle: ''});
-  }
+  };
 
-  render() {
-    return <div>
-      <h1>{this.state.authorName}</h1>
-      <button onClick={() => this.resetApplication()} className="reset">RESET</button>
-      <AuthorList authors={this.state.authors} selectAuthor={(author) => this.selectAuthor(author)} />
-      <Riddle riddle={this.state.riddle} author={this.state.authorLookup} play={this.state.play} />
-    </div>;
-  }
-}
+  return (
+    <div>
+      <h1>Cryptogram</h1>
+      <button onClick={() => resetApplication()} className="reset">
+        RESET
+      </button>
+      <Riddle
+        riddle={sonnets[Math.floor(Math.random() * sonnets.length - 1)]}
+      />
+    </div>
+  );
+};
 
 export default App;
--- src/components/riddle.js
+++ src/components/riddle.js
@@ -1,30 +1,23 @@
-import React, { Component } from 'react';
-import {alphabet} from '../helpers/alphabet';
+import React, { useEffect, useRef, useState } from "react";
+import { alphabet } from "../helpers/alphabet";
 
 // TODO Add Hints Button
-// TODO reveal title as a hint?
 
-class Riddle extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      riddle: '',
-      vanillaRiddle: '',
-      sortedAlpha: this.shuffleAlphabet()
-    };
-  }
+const Riddle = ({ riddle }) => {
+  const [parsedRiddle, setParsedRiddle] = useState(riddle.lines);
+  const [vanillaRiddle] = useState(null);
+  const originalRiddle = useRef("");
 
-  componentDidUpdate() {
-    if(this.props.riddle !== ""){
-      if(this.props.riddle.lines.join(" ") !== this.state.vanillaRiddle) {
-        this.renderRiddle();
-        this.setState({vanillaRiddle: this.props.riddle.lines.join(" ")});
-        this.setState({sortedAlpha: this.shuffleAlphabet()});
+  useEffect(() => {
+    if (riddle !== "") {
+      if (riddle.lines.join(" ") !== vanillaRiddle) {
+        renderRiddle();
+        originalRiddle.current = riddle.lines.join(" ");
       }
     }
-  }
+  }, []);
 
-  shuffleAlphabet() {
+  const shuffleAlphabet = () => {
     // create an alphabet array with real letters as keys and new letters as values
     // use this to create
     let doubleBet = alphabet.slice(0);
@@ -34,7 +27,7 @@ class Riddle extends Component {
       // get a random number from the new array
       var random = Math.floor(Math.random() * doubleBet.length);
 
-      if(alphabet[i] === doubleBet[random]) {
+      if (alphabet[i] === doubleBet[random]) {
         random = Math.floor(Math.random() * doubleBet.length);
       }
 
@@ -45,141 +38,173 @@ class Riddle extends Component {
       sorted.push(letter);
     }
     return sorted;
-  }
+  };
 
-  selectLetter(letter) {
+  const selectLetter = (letter) => {
     let allSame = document.getElementsByClassName(letter),
-        sameLength = allSame.length,
-        replacementLetter = '',
-        allSelected = document.getElementsByClassName("selected");
+      sameLength = allSame.length,
+      replacementLetter = "",
+      allSelected = document.getElementsByClassName("selected");
 
-    if(allSame[0].classList.contains("switched")) {
-      replacementLetter = allSame[0].dataset.letter
-      this.replaceLetter(allSame[0].innerHTML, true);
+    if (allSame[0].classList.contains("switched")) {
+      replacementLetter = allSame[0].dataset.letter;
+      replaceLetter(allSame[0].innerHTML, true);
     }
     // only do stuff if no others are selected
-    if (allSelected.length === 0){
+    if (allSelected.length === 0) {
       var i;
       for (i = 0; i < sameLength; i++) {
         // if this letter has already been switched
-        if(allSame[i].classList.contains("switched")) {
-        // want to instead turn it off and allow it to be played again
+        if (allSame[i].classList.contains("switched")) {
+          // want to instead turn it off and allow it to be played again
           allSame[i].classList.remove("switched");
           allSame[i].classList.remove("selected");
-        // to do so we must also unstrike the replaced letter
+          // to do so we must also unstrike the replaced letter
           allSame[i].innerHTML = replacementLetter;
         } else {
           allSame[i].classList.add("selected");
         }
       }
     } else {
-      for (i = sameLength-1; i >= 0; i--) {
+      for (i = sameLength - 1; i >= 0; i--) {
         allSame[i].classList.remove("selected");
       }
     }
-  }
+  };
 
-  renderEncryptedRiddle() {
+  const renderEncryptedRiddle = () => {
     let allSwitched = document.getElementsByClassName("letter");
     let encryptedRiddle = [];
     for (let i = 0; i < allSwitched.length; i++) {
-        encryptedRiddle.push(allSwitched[i].innerHTML);
+      encryptedRiddle.push(allSwitched[i].innerHTML);
     }
 
     encryptedRiddle = encryptedRiddle.join("").toLowerCase();
 
     return encryptedRiddle;
-  }
+  };
 
-  resetClasses() {
+  const resetClasses = () => {
     let allLetters = document.getElementsByClassName("letter");
     for (let i = 0; i < allLetters.length; i++) {
-        let trueLetter = allLetters[i].innerHTML;
-        console.log(trueLetter);
+      let trueLetter = allLetters[i].innerHTML;
+      console.log(trueLetter);
     }
-  }
+  };
 
-  replaceLetter(letter, removeStrike) {
+  const replaceLetter = (letter, removeStrike) => {
     // find the replacement letter element
-    let matches = document.querySelectorAll("[data-replace='" + letter.toUpperCase() + "']");
+    let matches = document.querySelectorAll(
+      "[data-replace='" + letter.toUpperCase() + "']"
+    );
     let allSelected = document.getElementsByClassName("selected");
-    let encryptedRiddle = '';
+    let encryptedRiddle = "";
 
     // check if already used - if so - skip and move on
-    if(removeStrike === false) {
-        // only move forward if there is a selected letter to replace
-        if (allSelected.length > 0){
-          // strike through the replacement letter
-          matches[0].classList.add("strike");
-          let selectedLength = allSelected.length - 1;
-          let i;
-          // count down through the list of selected letters and add switched
-          // also replace the current original letter
-          for (i = selectedLength; i >= 0; i--) {
-              let curletr = allSelected[i];
-              curletr.innerHTML = letter;
-              curletr.classList.remove("selected");
-              curletr.classList.add("switched");
-          }
-          encryptedRiddle = this.renderEncryptedRiddle();
+    if (removeStrike === false) {
+      // only move forward if there is a selected letter to replace
+      if (allSelected.length > 0) {
+        // strike through the replacement letter
+        matches[0].classList.add("strike");
+        let selectedLength = allSelected.length - 1;
+        let i;
+        // count down through the list of selected letters and add switched
+        // also replace the current original letter
+        for (i = selectedLength; i >= 0; i--) {
+          let curletr = allSelected[i];
+          curletr.innerHTML = letter;
+          curletr.classList.remove("selected");
+          curletr.classList.add("switched");
         }
-    // if already used and none are selected we're removing it
+        encryptedRiddle = renderEncryptedRiddle();
+      }
+      // if already used and none are selected we're removing it
     } else {
-        matches[0].classList.remove("strike");
+      matches[0].classList.remove("strike");
     }
     // Set up check to see if riddle has been solved
-    // this.props.riddle === this.state.vanillaRiddle
+    // riddle === vanillaRiddle
     // .replace(/['—’"?.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
-    if(this.state.vanillaRiddle.split(" ").join("").toLowerCase() === encryptedRiddle) {
+    if (
+      originalRiddle.current.split(" ").join("").toLowerCase() ===
+      encryptedRiddle
+    ) {
       document.getElementsByClassName("riddle")[0].classList.add("solved");
-      this.resetClasses();
+      resetClasses();
     }
-  }
+  };
 
-  splitRiddle(encrypted) {
+  const splitRiddle = (encrypted) => {
     let riddleHTML = [];
     // loop through words
-    for(let x = 0; x < encrypted.length; x++) {
+    for (let x = 0; x < encrypted.length; x++) {
       let doubledip = encrypted[x].toLowerCase().split(" ");
       let newPhrase = [];
 
       // loop through letters and render them into spans
-      for(let y = 0; y < doubledip.length; y++) {
+      for (let y = 0; y < doubledip.length; y++) {
         let current = doubledip[y];
         let newWord = [];
-        for(let nw = 0; nw < current.length; nw++) {
+        for (let nw = 0; nw < current.length; nw++) {
           let isPunctuation = alphabet.indexOf(current[nw].toUpperCase());
-          let newLetter = '';
+          let newLetter = "";
           let currentClass = "letter " + current[nw];
-          if(isPunctuation >= 0) {
-            newLetter = <span onClick={() => this.selectLetter(current[nw])} key={nw} data-letter={current[nw]} className={currentClass}>{current[nw]}</span>;
+          if (isPunctuation >= 0) {
+            newLetter = (
+              <span
+                onClick={() => selectLetter(current[nw])}
+                key={nw}
+                data-letter={current[nw]}
+                className={currentClass}
+              >
+                {current[nw]}
+              </span>
+            );
           } else {
-            newLetter = <span onClick={() => this.selectLetter(current[nw])} key={nw} data-letter={current[nw]} className="letter punctuation">{current[nw]}</span>;
+            newLetter = (
+              <span
+                onClick={() => selectLetter(current[nw])}
+                key={nw}
+                data-letter={current[nw]}
+                className="letter punctuation"
+              >
+                {current[nw]}
+              </span>
+            );
           }
           newWord.push(newLetter);
         }
-        newPhrase.push(<div key={y} className="word">{newWord}</div>);
+        newPhrase.push(
+          <div key={y} className="word">
+            {newWord}
+          </div>
+        );
       }
       // insert the words into phrases
-      riddleHTML.push(<div key={x*37} className="phrase">{newPhrase}</div>);
+      riddleHTML.push(
+        <div key={x * 37} className="phrase">
+          {newPhrase}
+        </div>
+      );
     }
-    this.setState({riddle: riddleHTML});
-  }
+    setParsedRiddle(riddleHTML);
+  };
 
-  encryptRiddle() {
+  const encryptRiddle = () => {
     let encrypted = [];
+    const sortedAlphabet = shuffleAlphabet();
 
-    for(var l = 0; l < this.props.riddle.lines.length; l++) {
-      let whatsMyLine = this.props.riddle.lines[l];
+    for (var l = 0; l < riddle.lines.length; l++) {
+      let whatsMyLine = riddle.lines[l];
       let encryptedLine = [];
 
       let splitEncrypted = whatsMyLine.toLowerCase().split("");
 
       // loop through the letters and replace them with new ones
-      for(var x = 0; x < splitEncrypted.length; x++) {
+      for (var x = 0; x < splitEncrypted.length; x++) {
         let letterIndex = alphabet.indexOf(splitEncrypted[x].toUpperCase());
-        if(letterIndex >= 0) {
-          encryptedLine.push(this.state.sortedAlpha[letterIndex]);
+        if (letterIndex >= 0) {
+          encryptedLine.push(sortedAlphabet[letterIndex]);
         } else {
           encryptedLine.push(splitEncrypted[x]);
         }
@@ -189,32 +214,40 @@ class Riddle extends Component {
 
     let encryptedRiddle = encrypted;
     return encryptedRiddle;
-  }
+  };
 
-  renderRiddle() {
+  const renderRiddle = () => {
     // encrypt the riddle
-    let newRiddle = this.encryptRiddle();
+    let newRiddle = encryptRiddle();
     // now split it into individual spans
-    this.splitRiddle(newRiddle);
-  }
+    splitRiddle(newRiddle);
+  };
 
-  render() {
-    if(this.props.riddle === '') {
-      return <div />
-    }
-    return  <div>
-        <div className="riddle">
-          {this.state.riddle}
-          <h2 className="author">&mdash; {this.props.author}</h2>
-        </div>
-        <div className="with">
-          <h2>Replace Letter With:</h2>
-            {alphabet.map((letter, i) => (
-              <span data-replace={letter} onClick={() => this.replaceLetter(letter.toLowerCase(), false)} key={letter}>{letter}</span>
-            ))}
-        </div>
-      </div>;
+  if (riddle === "") {
+    return <div />;
   }
-}
+  return (
+    <div>
+      <div className="with">
+        <h2>Replace Letter With:</h2>
+        <div className="alphabet">
+          {alphabet.map((letter) => (
+            <span
+              data-replace={letter}
+              onClick={() => replaceLetter(letter.toLowerCase(), false)}
+              key={letter}
+            >
+              {letter}
+            </span>
+          ))}
+        </div>
+      </div>
+      <div className="riddle">
+        {parsedRiddle}
+        <h2 className="author">&mdash; {riddle.author}</h2>
+      </div>
+    </div>
+  );
+};
 
 export default Riddle;
